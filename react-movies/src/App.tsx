3b02844fe4fe0ccc7c7ddaaa7a3595163193b286
Nara <narayanegn@gmail.com>
2021-10-21 08:43:04 -0300

    novo commit

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -1,16 +1,17 @@
-import IndividualMovie from './Movies/IndividualMovie';
 import { movieDTO } from './Movies/movies.model';
 
 function App() {
-  const testMovie: movieDTO = {
+  const inTheathers: movieDTO[] = [
+    {
     id: 1,
     title: 'O mágico de Oz',
     poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Wizard_of_oz_movie_poster.jpg/243px-Wizard_of_oz_movie_poster.jpg'
   }
+];
 
   return (
     <>
-      <IndividualMovie {...testMovie} ></IndividualMovie>
+      <h3>In theathers</h3>
     </>
   );
 }
