cd8df2c80b30cd8a1a9f18a4da39ded0579b8fe7
Nara <narayanegn@gmail.com>
2021-10-28 21:18:19 -0300

    form changes

--- react-movies/src/Genres/CreateGenres.tsx
+++ react-movies/src/Genres/CreateGenres.tsx
@@ -1,20 +1,33 @@
-import { useHistory } from 'react-router'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import TextField from 'Forms/TextField'
+import { Link } from 'react-router-dom'
 import Button from 'utils/Button'
+import * as Yup from 'yup'
 
 export default function CreateGenres() {
-  const history = useHistory()
+  //const history = useHistory()
 
   return (
     <>
       <h3>Create Genres</h3>
-      <Button
-        onClick={() =>
-          // saving in db
-          history.push('/genres')
-        }
+      <Formik
+        initialValues={{ name: '' }}
+        onSubmit={value => {
+          console.log(value)
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('This field is required')
+        })}
       >
-        Save changes
-      </Button>
+        <Form>
+          <TextField field="name" displayName="Name" />
+
+          <Button type="submit">Save changes</Button>
+          <Link className="btn btn-secudary" to="/genres">
+            Cancel
+          </Link>
+        </Form>
+      </Formik>
     </>
   )
 }
