4d8176a630a49ba2612b168923ec5d6c6d6d6ca3
mintfudge <narayanegn@gmail.com>
2021-11-12 09:38:58 -0300

    changes

--- react-movies/src/App.tsx
+++ react-movies/src/App.tsx
@@ -17,11 +17,11 @@ function App() {
           </Route>)}
         </Switch>
       </div>
-      <footer className="bd-footer py-5 mt-5 bg-light">
+      {/* <footer className="bd-footer py-5 mt-5 bg-light">
             <div className="container">
                 React Movies {new Date().getFullYear().toString()}
             </div>
-      </footer>
+      </footer> */}
     </BrowserRouter>
   )
 }
--- react-movies/src/Movies/movies.model.d.ts
+++ react-movies/src/Movies/movies.model.d.ts
@@ -1,10 +1,22 @@
-export interface movieDTO{
+export interface movieDTO {
   id: number;
   title: string;
   poster: string;
 }
 
-export interface landingPageDTO{
+export interface movieCreationDTO {
+  title: string;
+  inTheaters: boolean;
+  trailer: string;
+  releaseDate?: Date;
+  poster?: File;
+  posterURL?: string;
+  genresIds?: number[];
+  movieTheatersIds?: number[];
+  actors?: actorMovieDTO[];
+}
+
+export interface landingPageDTO {
   inTheaters?: movieDTO[];
   upcomingReleases?: movieDTO[];
 }
